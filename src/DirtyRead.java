7d36f0f33bc47948bf5a0f50b06ca24864a867a9
0868307 <0868307@hr.nl>
2015-05-29 16:09:42 +0200

    fck you thomas

---
+++ src/DirtyRead.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Wouter on 5/29/2015.
+ */
+public class DirtyRead {
+
+}
---
+++ src/Updater.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Wouter on 5/29/2015.
+ */
+public class Updater {
+
+    public Updater(){
+
+    }
+    public void read(){}
+
+    public void write(){}
+}
